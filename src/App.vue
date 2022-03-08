15d7bdebc369d9d283bd572a312412747223e959
Lin Da <0407da.lin@gmail.com>
2022-03-08 16:01:50 +0800

    Test bootstrap functionality and add nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <UserNavBar/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserNavBar from "./components/UserNavBar.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   UserNavBar
+  },
+  data() {
+    return {
+      formEntry:"",
+      title: "yoy",
+      nonsense:""
+    }
+  },
+  methods: {
+    change(x) {
+      this.title = x
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
