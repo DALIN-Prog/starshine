78c5a8890fdc7f9ab6d72c5e4824a9d804811df0
joannecya <e0421141@u.nus.edu>
2022-03-18 22:06:42 +0800

    new file names

---
+++ src/components/HealthSum.vue
@@ -0,0 +1,60 @@
+<template>
+  <!-- to include navbar -->
+
+  <h1 id="titleOfPage">Resident Health Summary</h1>
+  <div>
+    <div class="bloodP">
+      Blood Pressure<br>
+      <!-- {{BloodP}} -->
+    </div>
+
+    <div class="heartRate">
+      Heart Rate<br>
+      <!-- {{HeartR}} -->
+    </div>
+
+    <div class="temp">
+      Temperature<br>
+      <!-- {{tempData}} -->
+    </div>
+
+    <div class="respRate">
+      Respiratory Rate<br>
+      <!-- {{RespData}} -->
+    </div>
+
+    
+
+  </div>
+  
+  
+
+      
+</template>
+
+<script>
+export default {
+  name: "HealthSum",
+  
+
+
+}
+</script>
+
+<style>
+  #titleOfPage{
+    text-align: left;
+    border: 1px solid #dddddd;
+    padding: 15px;
+    border-radius: 3px;
+  }
+  .temp, .respRate, .bloodP, .heartRate{
+    display: block;
+    text-align: center;
+    border: 1px grey;
+    box-shadow: 2px 2px lightblue;
+  }
+
+
+
+</style>
---
+++ src/components/ResMgmt.vue
@@ -0,0 +1,66 @@
+<template>
+    <!-- to include navbar -->
+    <h1 id="titleOfPage">Resident Management</h1>
+    <h3 id="appt">Appointments #</h3>
+    <h3 id="res">Residents #</h3>
+    <div class="search" style="text-align:right;">
+        <input class="searchButton" type="text" placeholder="Search" aria-label="Search">
+        <button>Search</button>
+
+    </div>
+
+    <div class="resTable">
+        <table class="table table-striped table-sm">
+        <thead>
+            <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Phone No.</th>
+            <th scope="col">Blood Pressure</th>
+            <th scope="col">Heart Rate</th>
+            <th scope="col">Floor-Room Bed</th>
+            <th scope="col">Conditions</th>
+            <th scope="col">Admitted</th>
+            <th scope="col">Updated At</th>
+            <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        </table>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: "ResMgmt",
+    
+}
+</script>
+
+<style>
+    #titleOfPage, #appt, #res{
+        display: inline-block;
+        text-align:Left;
+        border-radius: 2px;
+    }
+
+    .resTable {
+        border-collapse:collapse;
+        width: 100%;
+        border:solid rgb(54, 93, 138);
+    }
+
+    th, td {
+    border: 1px solid #dddddd;
+    padding: 15px;
+}
+
+    tr:nth-child(even){
+        background-color: rgb(198, 229, 238);
+    }
+
+    .searchButton: hover{
+        background-color: blue;
+        border-radius: 1px;
+    }
+
+</style>
