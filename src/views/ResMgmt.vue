6758998e11ec4c7ae10b1680827933cce96cd0f2
joannecya <e0421141@u.nus.edu>
2022-03-25 16:57:16 +0800

    changes to resMgmt style

--- src/views/ResMgmt.vue
+++ src/views/ResMgmt.vue
@@ -1,14 +1,30 @@
 <template>
     <UserNavBar/>
-    <h1 id="titleOfPage">Resident Management</h1>
-    <h3 id="appt">Appointments No.</h3>
-    <h3 id="res">Residents No.</h3>
+    <div class="top">
+        <div id="titleOfPage">
+            <img src="@/assets/treatment.png" id="icon">
+            <h1>Resident Management</h1> 
+        </div>
+        
+        <div id="appt">
+            <img src="@/assets/appointment.png" id="icon">
+            <h3>Appointments</h3>
+        </div>
 
-    <div class="search" style="text-align:right;">
-        <input class="searchButton" type="text" placeholder="Search" aria-label="Search">
-        <button>Search</button>
+        <div id="res">
+            <img src="@/assets/people.png" id="icon">
+            <h3 >Residents</h3>
+        </div>
+
+        <div class="search">
+            <input class="searchInput" type="text" placeholder="Search" aria-label="Search">
+            <button id="searchBtn">Search</button>
+        </div>
 
     </div>
+    
+
+    <br>
 
     <div class="resTable">
         <table class="table table-striped table-sm">
@@ -32,15 +48,16 @@
 </template>
 
 <script>
-/* import { getAuth, unAuthStateChanged, signOut } from "firebase/auth"; */
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import UserNavBar from '@/components/UserNavBar.vue'
 
 export default {
     name: "ResMgmt",
-    components:
+    components: {
         UserNavBar
+    },
 
-    /* data () {
+    data () {
         return {
             user:false,
         }
@@ -53,21 +70,73 @@ export default {
                 this.user = user;
             }
         })
-    } */
+    }
 }
 </script>
 
 <style>
-    #titleOfPage, #appt, #res{
+    
+    .top{
+        display: grid;
+        grid-gap: 20px;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        margin: 5px 20px;
+        padding: 20px;
+    }
+
+    #titleOfPage{
         display: inline-block;
-        text-align:Left;
-        border-radius: 2px;
+        text-align: center;
+        float: left;
+        background-color:rgb(197, 230, 252);
+        color: black;
+        padding: 20px;
+        border-radius: 15px;
+    }
+
+    #icon{
+        width:75px;
+        height:75px;
+        float:left;
+    }
+
+    #appt, #res{
+        display: inline-block;
+        text-align: center;
+        font-weight: bold;
+        background-color: white;
+        padding: 20px;
+        margin: 10px 25px 10px 25px;
+        box-shadow: 3px 3px rgba(159, 163, 165, 0.651);
+        border-radius: 15px;
+    }
+
+    .searchInput{
+        border-color: rgb(211, 211, 211);
+        margin: 45px 25px 10px 0px;
+        padding: 5px 20px;
+        border-radius: 5px;
+    }
+
+    #searchBtn{
+        margin: 1px;
+        padding: 1px 10px;
+        color: rgb(91, 180, 131);
+        border-color: rgb(91, 180, 131);
+        border-radius: 5px;
+        font-size: 100%;
+    }
+
+    #searchBtn:hover{
+        box-shadow: 1px 1px rgb(91, 180, 131);
     }
 
     .resTable {
+        display: grid;
+        grid-gap: 20px;
         border-collapse:collapse;
-        width: 100%;
         border:solid rgb(54, 93, 138);
+        margin: 25px;
     }
 
     th, td {
@@ -79,9 +148,6 @@ export default {
         background-color: rgb(198, 229, 238);
     }
 
-    .searchButton: hover{
-        border-color: green;
-        border-radius: 1px;
-    }
+    
 
 </style>
