a72aa0eb1e821a63c8af6493d579c5fa88919b9b
Joel Tan <joeltanjh99@gmail.com>
2022-03-28 06:53:58 +0800

    Login and Account Creation

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LandingPage from "../views/LandingPage.vue";
 import Login from "../views/Login.vue";
+import AccountCreation from "../views/AccountCreation.vue";
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: "/login",
     name: "Login",
     component: Login,
+  },
+  {
+    path: "/AccountCreation",
+    name: "AccountCreation",
+    component: AccountCreation,
   },
 ];
 
