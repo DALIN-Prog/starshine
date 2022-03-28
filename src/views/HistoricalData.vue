3603ce3b7ad27bd65cf1c4108458542643a9cb2f
joannecya <e0421141@u.nus.edu>
2022-03-28 16:47:07 +0800

    add scope to style

--- src/views/HistoricalData.vue
+++ src/views/HistoricalData.vue
@@ -57,7 +57,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Sacramento&display=swap');
     #title {
         font-family: Merriweather;
