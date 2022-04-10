ac2aa9668160ea904d78be4cdbee8e3cda6b71ad
Lin Da <0407da.lin@gmail.com>
2022-04-10 08:12:28 +0800

    Fix Navbar for contact

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -49,13 +49,41 @@
 import AdminNavBar from "../components/AdminNavBar.vue"
 import ClientNavBar from "../components/ClientNavBar.vue"
 import NonLoginNavBar from "../components/NonLoginNavBar.vue"
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import firebaseApp from "../firebase.js"
+import { getFirestore, collection, getDocs } from "firebase/firestore"
+const db = getFirestore(firebaseApp)
 export default {
     name: "ContactUs",
     components: {
         AdminNavBar,
         ClientNavBar,
         NonLoginNavBar,
-  },
+    },
+    data() {
+        return {
+            user: false,
+            admin: false,
+        }
+    },
+    mounted() {
+        const auth = getAuth();
+        onAuthStateChanged(auth, async user => {
+            if (user) { // if logged in
+                this.user = auth.currentUser // set user to current user)
+                let documents = await getDocs(collection(db, "User"))
+                documents.forEach((docs) => {
+                    let data = docs.data()
+                    //console.log(docs.id)
+                    if (docs.id === this.user.uid) {
+                        console.log(data)
+                        this.admin = data.isAdmin
+                        //console.log(this.user);
+                    }
+                }) 
+            } 
+        })
+    }
 
 }
 </script>
